afdcb2fb7c684935d039105231fbc01889d70a20
Gary Stafford <gary.a.stafford@gmail.com>
2019-10-22 00:54:09 -0500

    Add .gitignore

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 // author: Gary A. Stafford
 // site: https://programmaticponderings.com
 // license: MIT License
-// purpose: Simple echo-based microservice: client
+// purpose: Simple echo-based microservice: nlp-client
 
 package main
 
 import (
 	"encoding/json"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-	"golang.org/x/net/context"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"golang.org/x/net/context"
 )
 
 func main() {
@@ -25,16 +27,36 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.Use(middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
-		return key == os.Getenv("AUTH_KEY"), nil
+	e.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		Skipper: func(c echo.Context) bool {
+			if strings.HasPrefix(c.Request().RequestURI, "/health") {
+				return true
+			}
+			return false
+		},
+		Validator: func(key string, c echo.Context) (bool, error) {
+			return key == os.Getenv("AUTH_KEY"), nil
+		},
 	}))
 
 	// Routes
+	e.GET("/health", getHealth)
 	e.POST("/keywords", extractKeywords)
 	e.POST("/tokens", extractTokens)
+	e.POST("/entities", extractEntities)
 
 	// Start server
 	e.Logger.Fatal(e.Start(port))
+}
+
+func getHealth(c echo.Context) error {
+	var response interface{}
+	err := json.Unmarshal([]byte(`{"status":"UP"}`), &response)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, response)
 }
 
 func extractKeywords(c echo.Context) error {
@@ -68,11 +90,27 @@ func extractTokens(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 
-	text := jsonMap["text"]
+	text := jsonMap["text"] // request body - 'text' json attribute value
 	tokens, err := getTokens(text.(string))
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 
 	return c.JSON(http.StatusOK, tokens)
+}
+
+func extractEntities(c echo.Context) error {
+	jsonMap := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	text := jsonMap["text"] // request body - 'text' json attribute value
+	entities, err := getEntities(text.(string))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, entities)
 }
--- prose.go
+++ prose.go
@@ -4,12 +4,18 @@ import (
 	"gopkg.in/jdkato/prose.v2"
 )
 
-// A Token represents an individual token of text such as a word or punctuation symbol.
+// A Token represents an individual Token of Text such as a word or punctuation symbol.
 // IOB format (short for inside, outside, beginning) is a common tagging format
 type Token struct {
-	Tag   string `json:"tag"`   // The token's part-of-speech tag.
-	Text  string `json:"text"`  // The token's actual content.
-	Label string `json:"label"` // The token's IOB label.
+	Tag   string `json:"Tag"`   // The Token's part-of-speech Tag.
+	Text  string `json:"Text"`  // The Token's actual content.
+	Label string `json:"Label"` // The Token's IOB Label.
+}
+
+// An Entity represents an individual named-entity.
+type Entity struct {
+	Text  string // The entity's actual content.
+	Label string // The entity's label.
 }
 
 func getTokens(phrase string) (interface{}, error) {
@@ -19,10 +25,30 @@ func getTokens(phrase string) (interface{}, error) {
 	}
 
 	var tokens []Token
-
-	for _, token := range doc.Tokens() {
-		tokens = append(tokens, Token{Tag: token.Tag, Text: token.Text, Label: token.Label})
+	for _, docToken := range doc.Tokens() {
+		tokens = append(tokens, Token{
+			Tag:   docToken.Tag,
+			Text:  docToken.Text,
+			Label: docToken.Label,
+		})
 	}
 
 	return tokens, nil
+}
+
+func getEntities(phrase string) (interface{}, error) {
+	doc, err := prose.NewDocument(phrase)
+	if err != nil {
+		return nil, err
+	}
+
+	var entities []Entity
+	for _, docEntities := range doc.Entities() {
+		entities = append(entities, Entity{
+			Text:  docEntities.Text,
+			Label: docEntities.Label,
+		})
+	}
+
+	return entities, nil
 }
