852acf1065cc3a1ac0ccb422c34c7ea6d864c142
Gary Stafford <gary.a.stafford@gmail.com>
2019-10-30 09:13:03 -0700

    Adding CodeBuild files

--- buildspec_files/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-export ISV_ACCOUNT
-export ISV_ECR_REGION
-$(aws ecr get-login --no-include-email --region ${ISV_ECR_REGION})
-docker build -t ${ISV_ACCOUNT}.dkr.ecr.${ISV_ECR_REGION}.amazonaws.com/nlp-client:1.0.0 . --no-cache
-docker push ${ISV_ACCOUNT}.dkr.ecr.${ISV_ECR_REGION}.amazonaws.com/nlp-client:1.0.0
